c5a0043a7d5606ca41b925e80045fd0e3e4f9255
Ayush Singh <ayushazuri@gmail.com>
2021-04-28 21:14:36 +0530

    Config changes

---
+++ SpotifyCredentials.md
@@ -0,0 +1,2 @@
+clientId: "a310eeb3a3a945bcb2e8e1a6761d9423",
+clientSecret: "59cfef6d18984f0ab861486d27e1f5b0",
---
+++ src/config.js
@@ -0,0 +1,9 @@
+const config = {
+	api: {
+		baseUrl: "https://api.spotify.com/v1",
+		authUrl: "https://accounts.spotify.com/api/token",
+		clientId: "",
+		clientSecret: "",
+	},
+};
+export default config;
