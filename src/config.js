c622a492aec786c25f937d0f49d28a562d4395ca
Ayush Singh <ayushazuri@gmail.com>
2021-04-28 21:18:21 +0530

    Config

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ const config = {
 	api: {
 		baseUrl: "https://api.spotify.com/v1",
 		authUrl: "https://accounts.spotify.com/api/token",
-		clientId: "",
-		clientSecret: "",
+		clientId: "a310eeb3a3a945bcb2e8e1a6761d9423",
+		clientSecret: "59cfef6d18984f0ab861486d27e1f5b0",
 	},
 };
 export default config;
